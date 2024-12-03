57c10806f691140cbed955985f250c7e366fc28d
Karoll Guzmán <karolldguzman@gmail.com>
2024-12-03 22:32:42 +0000

    creando footer

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer(props) {
+    return (
+        <footer className='footer text-center'>
+            <div className='container'>
+                <span className='text muted'>
+                Realizado por: Martin Martos, Luís Borjas, Alejandro Quiles y Karoll Guzmán.
+                </span>
+            </div>
+        </footer>
+    )
+
+}
+
+export default Footer;
